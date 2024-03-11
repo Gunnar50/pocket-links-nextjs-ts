d2b5d0f3fa20e033bd3b9946793700fa982959bb
Gustavo Passarella <gustavo.passarella@hotmail.com>
2024-03-11 11:43:57 +0000

    modal component

---
+++ src/app/Modal.tsx
@@ -0,0 +1,137 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { v4 as uuidv4 } from "uuid";
+import { z } from "zod";
+import {
+	addBookmark,
+	editBookmark,
+} from "../lib/features/bookmarks/bookmarksSlice";
+
+const formSchema = z.object({
+	// https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url
+	alias: z.string().max(50),
+	url: z
+		.string()
+		.regex(
+			/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+			"Invalid URL"
+		),
+});
+
+export interface Bookmark {
+	id: string;
+	alias: string;
+	url: string;
+}
+
+function Modal({
+	button,
+	title = "",
+	url = "",
+	id = "",
+}: {
+	button: () => React.ReactNode;
+	title?: string;
+	url?: string;
+	id?: string;
+}) {
+	const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+	const dispatch = useDispatch();
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			alias: title,
+			url: url,
+		},
+	});
+
+	function onSubmit(values: z.infer<typeof formSchema>) {
+		console.log(values);
+		const newId = id || uuidv4();
+		const newBookmark: Bookmark = { ...values, id: newId };
+		if (id && id.trim() !== "") {
+			// edit exisitng bookmark
+			dispatch(editBookmark(newBookmark));
+		} else {
+			// add new bookmark
+			dispatch(addBookmark(newBookmark));
+		}
+
+		form.reset();
+		setIsDialogOpen(false);
+	}
+	return (
+		<Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+			<DialogTrigger asChild>{button()}</DialogTrigger>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle className="mb-4"></DialogTitle>
+					<DialogDescription>
+						<Form {...form}>
+							<form
+								onSubmit={form.handleSubmit(onSubmit)}
+								className="space-y-8"
+							>
+								{/* TITLE INPUT */}
+								<FormField
+									control={form.control}
+									name="alias"
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Title/Alias</FormLabel>
+											<FormControl>
+												<Input placeholder="Your Title..." {...field} />
+											</FormControl>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+
+								{/* URL INPUT */}
+								<FormField
+									control={form.control}
+									name="url"
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>URL/Link</FormLabel>
+											<FormControl>
+												<Input placeholder="Your URL..." {...field} />
+											</FormControl>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+								<Button type="submit">Submit</Button>
+							</form>
+						</Form>
+					</DialogDescription>
+				</DialogHeader>
+			</DialogContent>
+		</Dialog>
+	);
+}
+
+export default Modal;
